29d139b6101ece2166e103c5920fe9284d9109e3
aree-vanier <gregory@ajayinkingston.com>
2019-08-05 21:41:31 -0400

    Added password hashing

--- users/local/process.php
+++ users/local/process.php
@@ -47,7 +47,7 @@ if ($_POST["mode"] == "register") {
     }
     $stmt->close();
 
-    $pass = $_POST["password"];
+    $pass = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
     // Add the users to the database
     $stmt = $conn->prepare("INSERT INTO tasks.users (name, email, password, ID) VALUES (?, ?, ?, ?)");
@@ -61,21 +61,21 @@ if ($_POST["mode"] == "register") {
 } else if ($_POST["mode"] == "login") {
     // If the user is signing in
     // Get users with same name/pass
-    $stmt = $conn->prepare("SELECT `ID` FROM `tasks`.`users` WHERE `email` LIKE ? AND `password` LIKE ?");
+    $stmt = $conn->prepare("SELECT ID,password FROM tasks.users WHERE email LIKE ?");
 
     echo "Email{".$_POST["email"]."}";
     echo "Password{".$_POST["password"]."}";
 
-    $stmt->bind_param("ss", $_POST['email'], $_POST['password']);
+    $stmt->bind_param("s", $_POST['email']);
     $stmt->execute();
-    $users = $stmt->get_result();
-    if (mysqli_num_rows($users) == 1) {
+    $user = $stmt->get_result()->fetch_assoc();
+    if (isset($user["ID"]) && password_verify($_POST["password"], $user["password"])) {
         // Set a cookie based on result
-        setcookie("token", $users->fetch_assoc()["ID"], time() + 12000000, "/");
+        setcookie("token", $user["ID"], time() + 12000000, "/");
         echo("Login successful");
         exit();
     } else {
-        echo("ERROR{Invalid email/password}");
+        echo("ERROR{Invalid email or password}");
     }
 } else {
     $err = "Mode failed";
